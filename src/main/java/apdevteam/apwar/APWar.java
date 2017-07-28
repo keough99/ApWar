db09dc678cfc2a15b44a7a88e20da4f308e45069
cccm5 <danieldavidson@aol.com>
2017-07-28 11:34:26 -0400

    Bug fixes and new features don't break if sender isn't a player account for several cases for which you should't be able to declare war colorize the factions in the war broadcast

--- src/main/java/apdevteam/apwar/APWar.java
+++ src/main/java/apdevteam/apwar/APWar.java
@@ -1,5 +1,6 @@
 package apdevteam.apwar;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.massivecraft.factions.Rel;
@@ -15,38 +16,38 @@ import org.bukkit.entity.Player;
 
 public class APWar extends JavaPlugin {
 
-	public void onEnable(){	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    public void onEnable(){	}
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(cmd.getName().equalsIgnoreCase("war")){
+            if(!(sender instanceof Player)){
+                sender.sendMessage("You can only use this command from in the game!");
+                return true;
+            }
+            Faction enemyFaction = FactionColl.get().getByName(args[0]); //get the enemy faction
+            MPlayer mplayer = MPlayer.get(sender);   //defines mplayer
+            Faction faction = mplayer.getFaction();   //get's sender faction
+            if(mplayer.getRole().isAtLeast(Rel.OFFICER)){
+                sender.sendMessage("You need to be at least an officer to do that");
+                return true;
+            }
+            if (faction.equals(enemyFaction)) {
+                sender.sendMessage("You can't declare war on your own faction!");
+                return true;
+            }
+            if(faction.getRelationTo(enemyFaction).equals(Rel.ENEMY)) {
+                sender.sendMessage("You're already at war with " + enemyFaction.getName());
+                return true;
+            }
+            faction.setRelationWish(enemyFaction, Rel.ENEMY);
+            for(Player p : Bukkit.getOnlinePlayers()){
+                p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 0);
+                MPlayer messagePlayer = MPlayer.get(p);
+                p.sendMessage(faction.getRelationTo(messagePlayer).getColor()  + faction.getName() + ChatColor.RESET + " has declared war on " +
+                        enemyFaction.getRelationTo(messagePlayer).getColor() + enemyFaction.getName());
+            }
 
-        Player player = (Player)sender;
-        
-       if(!(sender instanceof Player)){
-           sender.sendMessage("You can only use this command from in the game!");
-           return true;
-       }
-           
-           if(cmd.getName().equalsIgnoreCase("war")){
-               Faction enemyFaction = FactionColl.get().getByName(args[0]); //get the enemy faction
-               MPlayer mplayer = MPlayer.get(sender);   //defines mplayer
-               Faction faction = mplayer.getFaction();   //get's sender faction
-               
-                if(mplayer.getRole().isAtLeast(Rel.OFFICER)){
-                    if (faction == enemyFaction) {
-                        return false;
-                    }
-                    else {
-                    faction.setRelationWish(enemyFaction, Rel.ENEMY);
-                    
-                    for(Player p : Bukkit.getOnlinePlayers()){
-                    	p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 0);
-                    	}
-                    this.getServer().broadcastMessage(faction + " Has gone to war with " + enemyFaction + " !");
-                    }
-                    
-                       }
-           }
-		return true;
-	}
+        }
+        return false;
+    }
 }
